fc993de62b52449947f2cc9bedaf5d59608e43cb
Romain Boussot <rboussot@gmail.com>
2018-12-26 13:43:17 +0100

    partager une année complète + seed users

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,208 +13,208 @@ User.create!(
   firstname: "Mathieu",
   email: "mcoulon87@gmail.com",
   password: "goodvibrations",
-  admin: true)
+  admin: false)
 puts "."
 User.create!(
   lastname: "Dubois",
   firstname: "Maxime",
   email: "maxime.dubois1@free.fr",
   password: "goodvibrations",
-  admin: true)
+  admin: false)
 puts "."
 User.create!(
   lastname: "Conchon",
   firstname: "Amélie",
   email: "a.conchon.lablanche@gmail.com",
   password: "goodvibrations",
-  admin: true)
+  admin: false)
 puts "."
 User.create!(
   lastname: "Cattacin",
   firstname: "Brigitte",
   email: "cattacin.b@gmail.com",
   password: "goodvibrations",
-  admin: true)
+  admin: false)
 puts "."
 User.create!(
   lastname: "Pelletier",
   firstname: "Bernard",
   email: "bernard31pel@hotmail.fr",
   password: "goodvibrations",
-  admin: true)
+  admin: false)
 puts "."
 User.create!(
   lastname: "Cubillé",
   firstname: "Sophie",
   email: "sophie.cubille@free.fr",
   password: "goodvibrations",
-  admin: true)
+  admin: false)
 puts "."
 User.create!(
   lastname: "Ponsot",
   firstname: "Hélène",
   email: "helene.ponsot@free.fr",
   password: "goodvibrations",
-  admin: true)
+  admin: false)
 puts "."
 User.create!(
   lastname: "Gaëlle",
   firstname: "Lefirlec",
   email: "galinette22@gmail.com",
   password: "goodvibrations",
-  admin: true)
+  admin: false)
 puts "."
 User.create!(
   lastname: "Chavaroux",
   firstname: "Christophe",
   email: "chrispos@hotmail.com",
   password: "goodvibrations",
-  admin: true)
+  admin: false)
 puts "."
 User.create!(
   lastname: "Godard",
   firstname: "Luc",
   email: "godardluc@orange.fr",
   password: "goodvibrations",
-  admin: true)
+  admin: false)
 puts "."
 User.create!(
   lastname: "Duême",
   firstname: "Laure",
   email: "laure.dueme@gmail.com",
   password: "goodvibrations",
-  admin: true)
+  admin: false)
 puts "."
 User.create!(
   lastname: "Duême",
   firstname: "Cécile",
   email: "cecile.dueme@wanadoo.fr",
   password: "goodvibrations",
-  admin: true)
+  admin: false)
 puts "."
 User.create!(
   lastname: "Hervy",
   firstname: "William",
   email: "hervywilliam@yahoo.fr",
   password: "goodvibrations",
-  admin: true)
+  admin: false)
 puts "."
 User.create!(
   lastname: "Villatel",
   firstname: "Carole",
   email: "c.villatel1@orange.fr",
   password: "goodvibrations",
-  admin: true)
+  admin: false)
 puts "."
 User.create!(
   lastname: "Chauvet",
   firstname: "Claude",
   email: "claude.chauvet@intel.com",
   password: "goodvibrations",
-  admin: true)
+  admin: false)
 puts "."
 User.create!(
   lastname: "Billiard",
   firstname: "Pierre-Emmanuel",
   email: "bibs38000@hotmail.com",
   password: "goodvibrations",
-  admin: true)
+  admin: false)
 puts "."
 User.create!(
   lastname: "Billiard",
   firstname: "Nicolas",
   email: "nicolasbilliard.1978@free.fr",
   password: "goodvibrations",
-  admin: true)
+  admin: false)
 puts "."
 User.create!(
   lastname: "Billiard",
   firstname: "Karen",
   email: "karen.billiard@free.fr",
   password: "goodvibrations",
-  admin: true)
+  admin: false)
 puts "."
 User.create!(
   lastname: "Jean-Michel",
   firstname: "Ponsot",
   email: "poipoi.kom@free.fr",
   password: "goodvibrations",
-  admin: true)
+  admin: false)
 puts "."
 User.create!(
   lastname: "Julien",
   firstname: "Nicole",
   email: "nicole.julien20@gmail.com",
   password: "goodvibrations",
-  admin: true)
+  admin: false)
 puts "."
 User.create!(
   lastname: "Julien",
   firstname: "Jean-Stéphane",
   email: "jeanstephane.julien@gmail.com",
   password: "goodvibrations",
-  admin: true)
+  admin: false)
 puts "."
 User.create!(
   lastname: "Foulquier",
   firstname: "Jean-Michel",
   email: "jean-michel.foulquier@neuf.fr",
   password: "goodvibrations",
-  admin: true)
+  admin: false)
 puts "."
 User.create!(
   lastname: "Kinsky",
   firstname: "Nicolas",
   email: "nicolas-marie.kinsky@wanadoo.fr",
   password: "goodvibrations",
-  admin: true)
+  admin: false)
 puts "."
 User.create!(
   lastname: "Billiard",
   firstname: "Claire",
   email: "claire-billiard@orange.fr",
   password: "goodvibrations",
-  admin: true)
+  admin: false)
 puts "."
 User.create!(
   lastname: "Chauvet",
   firstname: "Nicole",
   email: "nicole.chauvet@hotmail.fr",
   password: "goodvibrations",
-  admin: true)
+  admin: false)
 puts "."
 User.create!(
   lastname: "Petrachi",
   firstname: "Thomas",
   email: "tpetrachizago@gmail.com",
   password: "goodvibrations",
-  admin: true)
+  admin: false)
 puts "."
 User.create!(
   lastname: "Seillans",
   firstname: "Thomas",
   email: "smokinglaslo@gmail.com",
   password: "goodvibrations",
-  admin: true)
+  admin: false)
 puts "."
 User.create!(
   lastname: "Girejensky",
   firstname: "Laetitia",
   email: "titipoussin2004@hotmail.com",
   password: "goodvibrations",
-  admin: true)
+  admin: false)
 puts "."
 User.create!(
   lastname: "Lamé",
   firstname: "Stéphane",
   email: "stephane.lame@live.fr",
   password: "goodvibrations",
-  admin: true)
+  admin: false)
 puts "."
 User.create!(
   lastname: "Pizzera",
   firstname: "Yvette",
   email: "yf@stratecollege.fr",
   password: "goodvibrations",
-  admin: true)
+  admin: false)
 puts "."
