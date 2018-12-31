c2522a89ce1752b5ce27a22edefe366010210a50
Romain Boussot <rboussot@gmail.com>
2018-12-31 17:03:15 +0100

    edit et show fullname

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,28 @@ class User < ApplicationRecord
          :recoverable, :validatable
   has_many :users_albums
   has_many :albums, through: :users_albums
+
   def name
     fullname
+  end
+
+  rails_admin do
+    list do
+      field :fullname
+    end
+  end
+
+  rails_admin do
+    edit do
+      field :email
+      field :password
+      field :fullname
+    end
+  end
+
+  rails_admin do
+    show do
+      field :fullname
+    end
   end
 end
